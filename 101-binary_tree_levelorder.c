3baa819fa8753eada830095284b4ccaabcf5e27f
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-08-03 14:55:12 -0700

    20. Level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -7,18 +7,32 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t a = 0;
-    size_t b = 0;
-
+    size_t left_height = 0;
+    size_t right_height = 0;
+    
     if (tree == NULL)
+    {
         return (0);
+    }
+    else
+    {
+        if (tree)
+        {
+            left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+            right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+        }
+        return ((left_height > right_height) ? left_height : right_height);
+    }
+}
 
-    if (tree->left)
-        a = 1 + binary_tree_height(tree->left);
-    if (tree->right)
-        b = 1 + binary_tree_height(tree->right);
-
-    return ((a > b) ? a : b);
+/**
+ * binary_tree_depth - depth of specified node from root
+ * @tree: node to check the depth
+ * Return: 0 is it is the root or number of depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
 
 /**
@@ -30,19 +44,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 void linked_node(link_t **head, const binary_tree_t *tree, size_t level)
 {
-    link_t *new, *aux;
-    new = malloc(sizeof(link_t));
-
-    if (new == NULL)
+    link_t *new_node, *aux;
+    
+    new_node = malloc(sizeof(link_t));
+    if (new_node == NULL)
+    {
         return;
-
-    new->n = level;
-    new->node = tree;
-
+    }
+    new_node->n = level;
+    new_node->node = tree;
+    
     if (*head == NULL)
     {
-        new->next = NULL;
-        *head = new;
+        new_node->next = NULL;
+        *head = new_node;
     }
     else
     {
@@ -51,8 +66,8 @@ void linked_node(link_t **head, const binary_tree_t *tree, size_t level)
         {
             aux = aux->next;
         }
-        new->next = NULL;
-        aux->next = new;
+        new_node->next = NULL;
+        aux->next = new_node;
     }
 }
 
@@ -65,11 +80,12 @@ void linked_node(link_t **head, const binary_tree_t *tree, size_t level)
  */
 void recursion(link_t **head, const binary_tree_t *tree)
 {
-    size_t level = 0;
+    size_t node_level = 0;
+    
     if (tree != NULL)
     {
-        level = binary_tree_height(tree);
-        linked_node(head, tree, level);
+        node_level = binary_tree_depth(tree);
+        linked_node(head, tree, node_level);
         recursion(head, tree->left);
         recursion(head, tree->right);
     }
@@ -84,34 +100,37 @@ void recursion(link_t **head, const binary_tree_t *tree)
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
     link_t *head, *aux;
-    size_t height = 0, count = 0;
-
+    size_t tree_height = 0, current_level = 0;
+    
     if (!tree || !func)
+    {
         return;
-
-    height = binary_tree_height(tree);
-    head = NULL;
-    recursion(&head, tree);
-
-    while (count <= height)
-    {
-        aux = head;
-        while (aux != NULL)
-        {
-            if (count == aux->n)
-            {
-                func(aux->node->n);
-            }
-            aux = aux->next;
-        }
-        count++;
     }
-
-    while (head != NULL)
+    else
     {
-        aux = head;
-        head = head->next;
-        free(aux);
+        tree_height = binary_tree_height(tree);
+        head = NULL;
+        recursion(&head, tree);
+        
+        while (current_level <= tree_height)
+        {
+            aux = head;
+            while (aux != NULL)
+            {
+                if (current_level == aux->n)
+                {
+                    func(aux->node->n);
+                }
+                aux = aux->next;
+            }
+            current_level++;
+        }
+        
+        while (head != NULL)
+        {
+            aux = head;
+            head = head->next;
+            free(aux);
+        }
     }
 }
-
